c67c63425d29819a48844b70e7aa4933267513cf
achyar10 <achyar.anshorie@gmail.com>
2022-08-28 13:29:32 +0700

    change new editor

--- resources/views/editor.blade.php
+++ resources/views/editor.blade.php
@@ -10,17 +10,18 @@
             background-image: none !important;
         }
     </style>
+    <link rel="stylesheet" href="{{ asset('frontend/vendor/pintura/pintura.css') }}">
+    <link rel="stylesheet" href="{{ asset('frontend/vendor/pintura/styles.css') }}">
     <div class="main-content container">
         <div class="content-wrapper row">
             <div class="col-12 content-header mb-4">
                 <h2 class="fw-bold">Tuliskan Pesan Spesialmu!</h2>
             </div>
             <div class="col-12 content-content">
+                <div data-card="{{ $row->id }}" data-id="{{ $member_id }}" id="image"
+                    data-image="{{ asset('/card/' . $row->image) }}" style="display: none;"></div>
                 <div class="container w-100 bg-light p-2 rounded mb-4">
-                    <div class="w-100 preview-image text-center" data-card="{{ $row->id }}" data-id="{{ $member_id }}"
-                        id="image" data-image="{{ asset('/card/' . $row->image) }}">
-                        <div id="editor_container"></div>
-                    </div>
+                    <div class="my-editor"></div>
                 </div>
             </div>
             <div class="row w-100 m-0">
@@ -34,123 +35,114 @@
             </div>
         </div>
     </div>
-    <script src="https://scaleflex.cloudimg.io/v7/plugins/filerobot-image-editor/latest/filerobot-image-editor.min.js">
-    </script>
-    <script>
+
+    <script type="module">
+        
+        import { appendDefaultEditor } from '../frontend/vendor/pintura/pintura.js';
+
         const dataImg = document.querySelector('#image');
         const preview = document.querySelector('#preview');
         let imageUrl = dataImg.getAttribute('data-image');
         let member_id = dataImg.getAttribute('data-id');
         let card_id = dataImg.getAttribute('data-card');
-        const {
-            TABS,
-            TOOLS
-        } = FilerobotImageEditor;
-        const config = {
-            source: imageUrl,
-            onSave: async (imageInfo, designState) => {
-                const file = imageInfo.imageBase64;
-                const ext = imageInfo.extension;
-                fetch('/api/upload', {
-                        method: 'POST',
-                        headers: {
-                            'Content-Type': 'application/json'
+        
+        const pintura = appendDefaultEditor(".my-editor", {
+            // The source image to load
+            src: imageUrl,
+            markupEditorToolbar: [
+                ['text', 'Text', { disabled: false }],
+                ['sharpie', 'Sharpie', { disabled: false }],
+                ['eraser', 'Eraser', { disabled: false }],
+                ['rectangle', 'Rectangle', { disabled: false }],
+            ],
+
+            // This will set a square crop aspect ratio
+            imageCropAspectRatio: 1,
+
+            // Stickers available to user
+            stickers: [
+                ["Emoji", ["⭐️", "😊", "👍", "👎", "☀️", "🌤", "🌥"]],
+                [
+                    "Markers",
+                    [
+                        {
+                            src: "sticker-one.svg",
+                            width: "5%",
+                            alt: "One"
                         },
-                        body: JSON.stringify({
-                            file,
-                            ext,
-                            member_id,
-                            card_id
-                        }),
-                    })
-                    .then(response => response.json())
-                    .then(data => {
-                        preview.style.display = 'block';
-                        preview.href = '/share/' + data.id;
-                        window.location.href = preview.href;
-                    })
-
-            },
-            annotationsCommon: {
-                fill: '#ff0000',
-            },
-            Text: {
-                text: 'Edit text here...'
-            },
-            Rotate: {
-                angle: 90,
-                componentType: 'slider'
-            },
-            translations: {
-                profile: 'Profile',
-                coverPhoto: 'Cover photo',
-                facebook: 'Facebook',
-                socialMedia: 'Social Media',
-                fbProfileSize: '1080x1080px',
-                fbCoverPhotoSize: '820x312px',
-            },
-            Crop: {
-                presetsItems: [{
-                        titleKey: 'Post Instagram',
-                        descriptionKey: '1:1',
-                        ratio: 1 / 1,
-                    },
-                    {
-                        titleKey: 'cinemascope',
-                        descriptionKey: '21:9',
-                        ratio: 21 / 9,
-                    },
-                ],
-                presetsFolders: [{
-                    titleKey: 'socialMedia',
-                    groups: [{
-                        titleKey: 'instagram',
-                        items: [{
-                                titleKey: 'post',
-                                width: 1080,
-                                height: 1080,
-                                descriptionKey: 'fbProfileSize',
-                            },
-                            {
-                                titleKey: 'coverPhoto',
-                                width: 820,
-                                height: 312,
-                                descriptionKey: 'fbCoverPhotoSize',
-                            },
-                        ],
-                    }, ],
-                }, ],
-            },
-            tabsIds: [TABS.ANNOTATE, TABS.FILTERS, ], // or ['Adjust', 'Annotate', 'Watermark']
-            defaultTabId: TABS.ANNOTATE, // or 'Annotate'
-            defaultToolId: TOOLS.TEXT, // or 'Text'
-        };
-
-        // Assuming we have a div with id="editor_container"
-        const filerobotImageEditor = new FilerobotImageEditor(
-            document.querySelector('#editor_container'),
-            config,
-        );
-
-        filerobotImageEditor.render({
-            onClose: (closingReason) => {
-                console.log('Closing reason', closingReason);
-                filerobotImageEditor.terminate();
-            },
+                        {
+                            src: "sticker-two.svg",
+                            width: "5%",
+                            alt: "Two"
+                        },
+                        {
+                            src: "sticker-three.svg",
+                            width: "5%",
+                            alt: "Three"
+                        }
+                    ]
+                ]
+            ],
+            locale: { labelButtonExport: 'SIMPAN' },
+            utils: [
+                    'annotate',
+                    'sticker',
+                    'crop',
+                    'filter',
+                    'finetune',
+                    'frame',
+                    'resize',
+            ],
         });
 
-        async function share() {
-            try {
-                await navigator.share({
-                    title: 'Hello',
-                    text: 'Check out this image!',
-                    files: [files],
-                });
-                console.log('shared successfully')
-            } catch (err) {
-                alert(`Your system doesn't support sharing these files.`)
-            }
-        }
-    </script>
+        pintura.on('process', async (imageState) => {
+            const file = await toBase64(imageState.dest);
+            const ext = imageState.dest.type.split('/');
+            fetch('/api/upload', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({
+                        file,
+                        ext: ext[1],
+                        member_id,
+                        card_id
+                    }),
+                })
+                .then(response => response.json())
+                .then(data => {
+                    preview.style.display = 'block';
+                    preview.href = '/share/' + data.id;
+                    // window.location.href = preview.href;
+                })
+        });
+
+        const downloadFile = (file) => {
+            // Create a hidden link and set the URL using createObjectURL
+            const link = document.createElement('a');
+            link.style.display = 'none';
+            link.href = URL.createObjectURL(file);
+            link.download = file.name;
+
+            // We need to add the link to the DOM for "click()" to work
+            document.body.appendChild(link);
+            link.click();
+
+            // To make this work on Firefox we need to wait a short moment before clean up
+            setTimeout(() => {
+                URL.revokeObjectURL(link.href);
+                link.parentNode.removeChild(link);
+            }, 0);
+        };
+
+        const toBase64 = file => new Promise((resolve, reject) => {
+            const reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = () => resolve(reader.result);
+            reader.onerror = error => reject(error);
+        });
+        </script>
+
 
 @endsection
