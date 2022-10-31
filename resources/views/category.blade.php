7b2cd73036f372a25c25b2a30ef4d774d49fded3
Dewa Andhika Putra <dewaandhika18@gmail.com>
2022-10-31 09:25:47 +0700

    Fix carousel controls and chocochip pulse

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -6,13 +6,13 @@
             <img class="section__img" src="{{ asset('frontend/images/bg/bg.png') }}" alt="Rainforest view with sunset" />
         </div>
         <div class="section__float_bg">
-            <img class="section__img chocochip chocochip1" src="{{ asset('frontend/images/decorations/chocochip2.png') }}" />
-            <img class="section__img chocochip chocochip2" src="{{ asset('frontend/images/decorations/chocochip.png') }}" />
-            <img class="section__img chocochip chocochip3"
+            <img class="section__img chocochip chocochip1 animate__animated animate__infinite" src="{{ asset('frontend/images/decorations/chocochip2.png') }}" />
+            <img class="section__img chocochip chocochip2 animate__animated animate__infinite" src="{{ asset('frontend/images/decorations/chocochip.png') }}" />
+            <img class="section__img chocochip chocochip3 animate__animated animate__infinite"
                 src="{{ asset('frontend/images/decorations/chocochip2.png') }}" />
-            <img class="section__img chocochip chocochip4"
+            <img class="section__img chocochip chocochip4 animate__animated animate__infinite"
                 src="{{ asset('frontend/images/decorations/chocochip2.png') }}" />
-            <img class="section__img chocochip chocochip5" src="{{ asset('frontend/images/decorations/chocochip.png') }}" />
+            <img class="section__img chocochip chocochip5 animate__animated animate__infinite" src="{{ asset('frontend/images/decorations/chocochip.png') }}" />
         </div>
         <section class="section">
             <div class="section__content container">
