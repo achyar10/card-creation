7dfdc7f89278aebd3825b05e09ff07312a949c22
Dewa Andhika Putra <dewaandhika18@gmail.com>
2022-11-02 21:04:57 +0700

    Change desc text

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,8 +27,7 @@
                 <div class="col-12 text-center">
                     <img class="img-fluid home__logo animate__animated"
                         src="{{ asset('frontend/images/icons/goodtime_gift_logo.png') }}">
-                    <p class="text-white lh-lg mb-4">Kamu bisa berkreasi seru membuat ucapan untuk diberikan<br>kepada
-                        orang spesial di momen-momen istimewa</p>
+                    <p class="text-white lh-lg mb-4">Kamu bisa berkreasi seru membuat ucapan untuk diberikan<br>kepada orang tersayang dan teman-temanmu di momen-momen istimewa</p>
                     <div class="cta-button">
                         <button type="button" class="btn btn-cust-white mb-3" data-bs-toggle="modal"
                             data-bs-target="#rulesModal">Cara Ikutan</button>
@@ -111,7 +110,7 @@
                                 <img class="img-fluid home__logo"
                                     src="{{ asset('frontend/images/icons/goodtime_gift_logo.png') }}">
                             </div>
-                            <h3 class="my-4">Kumpulin Poin Reward Yuk!</h3>
+                            <h3 class="my-4">Poin Rewardmu</h3>
                             <div class="point__counter mb-4">
                                 @foreach ($points as $point)
                                     <div class="point__digit_wrapper">
