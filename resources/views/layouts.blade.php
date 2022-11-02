e080e4a4cface8ce089c84748ebb8840db1d0a51
Dewa Andhika Putra <dewaandhika18@gmail.com>
2022-11-02 17:38:39 +0700

    Bump version

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -24,7 +24,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
     <link href="{{ asset('frontend/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link href="{{ asset('frontend/styles/style.css?v=1.0.6') }}" rel="stylesheet">
+    <link href="{{ asset('frontend/styles/style.css?v=1.0.7') }}" rel="stylesheet">
     <link rel="shortcut icon" href="{{ asset('assets/images/favicon.ico') }}">
 </head>
 
@@ -75,7 +75,7 @@
 
     <script src="{{ asset('frontend/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('frontend/js/custom.js?v=1.0.1') }}"></script>
-    <script src="{{ asset('frontend/js/carousel.js?v=1.0.0') }}"></script>
+    <script src="{{ asset('frontend/js/carousel.js?v=1.0.1') }}"></script>
 </body>
 
 </html>
