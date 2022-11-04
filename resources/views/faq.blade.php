0f98314cfa361f35f52df7e1cf5e8387a04d7d09
Dewa Andhika Putra <dewaandhika18@gmail.com>
2022-11-04 23:12:11 +0700

    Remove chocochip

--- resources/views/faq.blade.php
+++ resources/views/faq.blade.php
@@ -6,22 +6,6 @@
         <div class="section__bg">
             <img class="section__img" src="{{ asset('frontend/images/bg/bg.png') }}" alt="Rainforest view with sunset" />
         </div>
-        <div class="section__float_bg d-none">
-            <img class="section__img chocochip chocochip1 animate__animated"
-                src="{{ asset('frontend/images/decorations/chocochip2.png') }}" />
-            <img class="section__img chocochip chocochip2 animate__animated"
-                src="{{ asset('frontend/images/decorations/chocochip.png') }}" />
-            <img class="section__img chocochip chocochip3 animate__animated"
-                src="{{ asset('frontend/images/decorations/chocochip2.png') }}" />
-            <img class="section__img chocochip chocochip4 animate__animated"
-                src="{{ asset('frontend/images/decorations/chocochip2.png') }}" />
-            <img class="section__img chocochip chocochip5 animate__animated"
-                src="{{ asset('frontend/images/decorations/chocochip.png') }}" />
-            <img class="section__img chocochip chocochip6 animate__animated"
-                src="{{ asset('frontend/images/decorations/chocochip2.png') }}" />
-            <img class="section__img chocochip chocochip7 animate__animated"
-                src="{{ asset('frontend/images/decorations/chocochip.png') }}" />
-        </div>
 
         <section class="section" id="">
             <div class="section__content container">
