deab4cc3d0376174ccc5f651e2d09bb3f593bfe0
Dewa Andhika Putra <dewaandhika18@gmail.com>
2022-11-15 10:17:31 +0700

    Optimize youtube iframe load

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -53,8 +53,10 @@
                 <div class="modal-body gt-modal-body">
                     <div class="gt-video text-center">
                         <iframe width="100%" height="300" src="https://www.youtube.com/embed/w34eb8F0_88"
-                            title="YouTube video player" frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube.com/embed/w34eb8F0_88?autoplay=1><img src=https://img.youtube.com/vi/w34eb8F0_88/hqdefault.jpg alt='Cara Ikutan GoodTime Gift Card'><span>▶</span></a>"
+                            title="Cara Ikutan GoodTime Gift Card"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
                             allowfullscreen></iframe>
                     </div>
                     <h4 class="my-4">Cara Ikutan</h4>
